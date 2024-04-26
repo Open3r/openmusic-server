3a37f0b77bb716df02fdc921646e6d1eda614c21
전민오 <jbj338033@gmail.com>
2024-04-26 23:13:16 +0900

    cicd test

--- src/main/kotlin/com/open3r/openmusic/index/controller/IndexController.kt
+++ src/main/kotlin/com/open3r/openmusic/index/controller/IndexController.kt
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController
 class IndexController {
     @GetMapping
     fun getIndex(): String {
-        return "Index 123123123"
+        return "Index 1231231233"
     }
 }
