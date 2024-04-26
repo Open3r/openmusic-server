c79d0335037ae8d4b1f824aac108ebb7c73f9de7
전민오 <jbj338033@gmail.com>
2024-04-26 10:59:13 +0900

    refactor: User

--- src/main/kotlin/com/open3r/openmusic/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/open3r/openmusic/domain/user/controller/UserController.kt
@@ -2,6 +2,7 @@ package com.open3r.openmusic.domain.user.controller
 
 import com.open3r.openmusic.domain.user.dto.response.UserResponse
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,6 +16,14 @@ class UserController {
 
     @GetMapping("/me")
     fun getMe(): UserResponse {
+        return UserResponse(
+            "Joe da bovo",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5ay_I4fQMfRIknHYt_iva3_-Hmat46b0Fh1HvUOXlNw&s"
+        )
+    }
+
+    @PostMapping
+    fun createUser(): UserResponse {
         return UserResponse(
             "Joe da bovo",
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5ay_I4fQMfRIknHYt_iva3_-Hmat46b0Fh1HvUOXlNw&s"
--- src/main/kotlin/com/open3r/openmusic/domain/user/service/UserServiceImpl.kt
+++ src/main/kotlin/com/open3r/openmusic/domain/user/service/UserServiceImpl.kt
@@ -3,5 +3,4 @@ package com.open3r.openmusic.domain.user.service
 import org.springframework.stereotype.Service
 
 @Service
-class UserServiceImpl {
-}
+class UserServiceImpl
