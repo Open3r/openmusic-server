7fc98d8f41c499c5d00bad828049fe224c1ab11d
전민오 <jbj338033@gmail.com>
2024-04-27 17:33:52 +0900

    fix: Make BaseEntity abstraction

--- src/main/kotlin/com/open3r/openmusic/global/common/BaseEntity.kt
+++ src/main/kotlin/com/open3r/openmusic/global/common/BaseEntity.kt
@@ -10,7 +10,7 @@ import java.time.LocalDateTime
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
-class BaseEntity(
+abstract class BaseEntity(
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     var createdAt: LocalDateTime? = null,
