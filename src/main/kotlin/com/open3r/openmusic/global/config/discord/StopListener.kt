d20d7c7aec759236c9fd5a4b309742af1477172e
전민오 <jbj338033@gmail.com>
2024-08-01 14:48:29 +0900

    feat: swagger

--- src/main/kotlin/com/open3r/openmusic/global/config/discord/StopListener.kt
+++ src/main/kotlin/com/open3r/openmusic/global/config/discord/StopListener.kt
@@ -2,21 +2,21 @@ package com.open3r.openmusic.global.config.discord
 
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDA
-import org.springframework.boot.context.event.ApplicationStartedEvent
 import org.springframework.context.ApplicationListener
+import org.springframework.context.event.ContextClosedEvent
 import org.springframework.stereotype.Component
 import java.time.Instant
 
 @Component
-class StartListener(
+class StopListener(
     private val jda: JDA,
     private val discordProperties: DiscordProperties
-): ApplicationListener<ApplicationStartedEvent> {
-    override fun onApplicationEvent(event: ApplicationStartedEvent) {
+): ApplicationListener<ContextClosedEvent> {
+    override fun onApplicationEvent(event: ContextClosedEvent) {
         jda.getTextChannelById(discordProperties.channelId)?.sendMessageEmbeds(
             EmbedBuilder()
-                .setTitle("서버가 시작되었습니다.")
-                .setColor(0x00FF00)
+                .setTitle("서버가 종료되었습니다.")
+                .setColor(0xFF0000)
                 .setTimestamp(Instant.now())
                 .build()
         )?.queue()
